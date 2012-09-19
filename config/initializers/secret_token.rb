28b24f3b5565f0d6b6911d779650cbd3a8a6d59f
Robert Marshall <robert.marshallii1@gmail.com>
2012-09-19 11:42:29 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gimmewx::Application.config.secret_token = '4aee3f4af722a5ad275702af4e3a5008d2499d4073557fc131da97e8715d5caf59fc852e941f6068d6872c002c531704f134aa07b69eb18c6dcf401d38974991'
